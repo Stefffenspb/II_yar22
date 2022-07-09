cbf77b984a8d8dfaad5a7a5bd795a338fe496c85
Stefffenspb <108985618+Stefffenspb@users.noreply.github.com>
2022-07-09 23:10:18 +0300

    Add files via upload

--- scratch_2.py
+++ scratch_2.py
@@ -2,17 +2,28 @@
 import pandas as pd
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 from sklearn.metrics import recall_score
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import Normalizer
+
+
 #Подготовим данные
 
 
@@ -26,10 +37,19 @@ def data_prepare(df):
     df = df.drop(columns='Время засыпания')
     df = df.drop(columns='Время пробуждения')
     #В данных нет Перекоса
-    df = df.drop(columns='Этнос')
+    #df = df.drop(columns='Этнос')
     df = df.drop(columns='Религия')
     df = df.drop(columns='Профессия')
+
+
+    #df = df.drop(columns='Онкология')
+    df = df.drop(columns='ВИЧ/СПИД')
+    #df = df.drop(columns='Туберкулез легких ')
+    df = df.drop(columns='Образование')
     #Преобразуем справочники
+    #df = df.replace({'Национальность': {'Русские': '1', 'Татары': '2','Таджики': '3','Немцы': '4','Евреи': '5','Азербайджанцы':'6','Другие национальности':'7','Чуваши':'8','Армяне':'9',
+    #                                    'Украинцы':'12','Эстонцы':'13','Молдаване':'14','Мордва':'15','Киргизы':'16','Казахи':'17','Белорусы':'18','Башкиры':'19','Буряты':'20',
+    #                                    'Удмурты':'21','Лезгины':'22'}})
     #df = df.replace({'Профессия': {'дипломированные специалисты': '1',
     #                               'квалифицированные работники сельского хозяйства и рыболовного': '2',
     #                               'низкоквалифицированные работники': '3',
@@ -39,36 +59,40 @@ def data_prepare(df):
     #                               'представители   законодат.   органов   власти,  высокопостав. долж.лица и менеджеры': '7',
     #                               'служащие': '8', 'операторы и монтажники установок и машинного оборудования': '9',
     #                               'техники и младшие специалисты': '10', 'вооруженные силы': '11'}})
-   # df = df.replace({'Этнос': {'европейская': '1',
-   #                            'прочее (любая иная этно-расовая группа, не представленная выше)': '2',
-   #                            'другая азиатская (Корея, Малайзия, Таиланд, Вьетнам, Казахстан, Киргизия, Туркмения, Узбекистан, Таджикистан)': '3'}})
-    df = df.replace({'Пол': {'М': '0', 'Ж': '1'}})
-   # df = df.replace({'Религия': {'Христианство': '1', 'Атеист / агностик': '2', 'Нет': '2', 'Ислам': '1', 'Другое': '1',
-   #                              'Индуизм': '1'}})
-    df = df.replace({'Образование': {'5 - ВУЗ': '5', '4 - профессиональное училище': '4',
-                                     '3 - средняя школа / закон.среднее / выше среднего': '3',
-                                     '2 - начальная школа': '2'}})
+    df = df.replace({'Этнос': {'европейская': '1',
+                               'прочее (любая иная этно-расовая группа, не представленная выше)': '2',
+                               'другая азиатская (Корея, Малайзия, Таиланд, Вьетнам, Казахстан, Киргизия, Туркмения, Узбекистан, Таджикистан)': '3'}})
+    df = df.replace({'Пол': {'М': '1', 'Ж': '0'}})
+    #df = df.replace({'Религия': {'Христианство': '1', 'Атеист / агностик': '2', 'Нет': '0', 'Ислам': '3', 'Другое': '4',
+    #                             'Индуизм': '5'}})
+    #df = df.replace({'Образование': {'5 - ВУЗ': '5', '4 - профессиональное училище': '4',
+    #                                 '3 - средняя школа / закон.среднее / выше среднего': '3',
+    #                                 '2 - начальная школа': '2'}})
     df = df.replace({'Семья': {'в браке в настоящее время': '0', 'в разводе': '1',
                                'гражданский брак / проживание с партнером': '2', 'вдовец / вдова': '3',
                                'никогда не был(а) в браке': '4',
                                'раздельное проживание (официально не разведены)': '5'}})
     df = df.replace(
         {'Статус Курения': {'Курит': '3', 'Никогда не курил(а)': '1', 'Бросил(а)': '2', 'Никогда не курил': '1'}})
-    df = df.replace({'Частота пасс кур': {'1-2 раза в неделю': '1', '3-6 раз в неделю': '1',
-                                          'не менее 1 раза в день': '2', '4 и более раз в день': '2',
-                                          '2-3 раза в день': '2'}})
+    df = df.replace({'Частота пасс кур': {'1-2 раза в неделю': '1', '3-6 раз в неделю': '2',
+                                          'не менее 1 раза в день': '3', '4 и более раз в день': '4',
+                                          '2-3 раза в день': '5'}})
     df = df.replace(
         {'Алкоголь': {'употребляю в настоящее время': '3', 'никогда не употреблял': '1', 'ранее употреблял': '2'}})
-    df['Difference'] = df['Difference'].mask(df['Difference'] > 9, 2)
-    df['Difference'] = df['Difference'].mask((df['Difference'] >= 7) & (df['Difference'] <= 9), 1)
     df['Difference'] = df['Difference'].mask(df['Difference'] < 7, 0)
+
+    df['Difference'] = df['Difference'].mask((df['Difference'] >= 7) & (df['Difference'] <= 9), 1)
+    df['Difference'] = df['Difference'].mask(df['Difference'] > 9, 2)
     df['Возраст алког'] = df['Возраст алког'].mask(df['Возраст алког'] > 3, 1)
+    df['Сигарет в день'] = df['Сигарет в день'].mask(df['Сигарет в день'] < 7, 1)
+    df['Сигарет в день'] = df['Сигарет в день'].mask((df['Сигарет в день'] >= 7) & (df['Difference'] < 20), 2)
     df['Сигарет в день'] = df['Сигарет в день'].mask(df['Сигарет в день'] >= 20, 3)
-    df['Сигарет в день'] = df['Сигарет в день'].mask((df['Сигарет в день'] >= 5) & (df['Difference'] < 20), 2)
-    df['Сигарет в день'] = df['Сигарет в день'].mask(df['Сигарет в день'] < 5, 1)
-    df['Возраст курения'] = df['Возраст курения'].mask(df['Возраст курения'] >= 20, 3)
-    df['Возраст курения'] = df['Возраст курения'].mask((df['Возраст курения'] >= 10) & (df['Difference'] < 20), 2)
+
     df['Возраст курения'] = df['Возраст курения'].mask(df['Возраст курения'] < 10, 1)
+    df['Возраст курения'] = df['Возраст курения'].mask((df['Возраст курения'] >= 10) & (df['Difference'] < 20), 2)
+    df['Возраст курения'] = df['Возраст курения'].mask(df['Возраст курения'] >= 20, 3)
+
+
     df['Пол'] = df['Пол'].fillna('0')
 
     df['Частота пасс кур'] = df['Частота пасс кур'].fillna('0')
@@ -80,46 +104,69 @@ def data_prepare(df):
     return df
 
 #Обучение и Прогнозирование
-def science(col, preds, filecsv_from, filecsv_to):
+def science(col, preds, filecsv_from, filecsv_to,colim):
 
+    df_train = df
+    for diag in colim:
 
+        df_train.drop(columns=diag)
 
-    X = df.drop(col, axis=1)
-    y = df[col]
-    X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.5, random_state=1)
+    X = df_train.drop(col, axis=1)
+    y = df_train[col]
+    test_size = 0.2
+    seed = 7
+    X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=test_size, random_state=seed)
+    num_folds = 10
+    n_estimators = 100
+    scoring = 'recall'
     models = []
     #Список Моделей
-    models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+
+    models.append(('NB', GaussianNB()))
     models.append(('LDA', LinearDiscriminantAnalysis()))
     models.append(('KNN', KNeighborsClassifier()))
     models.append(('CART', DecisionTreeClassifier()))
-    models.append(('NB', GaussianNB()))
-    models.append(('SVM', SVC(gamma='auto')))
+    models.append(('MLP', MLPClassifier()))
+    models.append(('BG', BaggingClassifier(n_estimators=n_estimators)))
+    models.append(('RF', RandomForestClassifier(n_estimators=n_estimators)))
+    models.append(('ET', ExtraTreesClassifier(n_estimators=n_estimators)))
+    models.append(('AB', AdaBoostClassifier(n_estimators=n_estimators, algorithm='SAMME')))
+    models.append(('GB', GradientBoostingClassifier(n_estimators=n_estimators)))
 
 
 
 
     # оцениваем модель на каждой итерации
-    results = []
+    scores = []
     names = []
+    results = []
+    predictions = []
+    msg_row = []
     best = []
     for name, model in models:
-        kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-        cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='recall')
-        results.append(cv_results)
+        kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
+        cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
         names.append(name)
-        #Оценка Моделей
-        print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+        results.append(cv_results)
+        m_fit = model.fit(X_train, Y_train)
+        m_predict = model.predict(X_validation)
+        predictions.append(m_predict)
+        m_score = model.score(X_validation, Y_validation)
+        scores.append(m_score)
+        msg = "%s: train = %.3f (%.3f) / test = %.3f" % (name, cv_results.mean(), cv_results.std(), m_score)
+        msg_row.append(msg)
+        print(msg)
         best.append(cv_results.mean())
+        modi = model.fit(X, y)
+        predi = modi.predict(X)
+        predi = pd.DataFrame({col: predi})
+        recall = recall_score(y, predi)
+        print('recall_score', recall)
 
     index = best.index(max(best))
     #Выберем Лучшую
     best_train = models[index][1]
-    modi = best_train.fit(X, y)
-    predi = modi.predict(X)
-    predi = pd.DataFrame({col: predi})
-    recall = recall_score(y, predi)
-    print(recall)
+
     df_test = pd.read_csv('data/test_dataset_test.csv')
     df_test.head()
     df_test_2 = pd.read_csv(filecsv_to)
@@ -131,8 +178,11 @@ def science(col, preds, filecsv_from, filecsv_to):
     df_merged = df_merged.drop(columns='ID_y')
     df_merged_id = pd.read_csv(filecsv_to)
     df_merged_id.head()
+
     df_merged = df_merged.drop(columns=col)
     df_merged_d = df_merged.drop(columns='ID')
+    for diag in colim:
+        df_merged_d.drop(columns=diag)
     gnb = best_train
 
     model = gnb.fit(X,  y)
@@ -140,6 +190,7 @@ def science(col, preds, filecsv_from, filecsv_to):
     preds = model.predict(df_merged_d)
 
     preds = pd.DataFrame({col: preds})
+
     df_merged_id = df_merged_id.drop(columns=col)
     df_merged_id = pd.concat([df_merged_id, preds], axis=1)
 
@@ -163,7 +214,7 @@ print(df.skew())
 # scatter_matrix(df)
 # plt.show()
 preds = []
-gnb = LinearDiscriminantAnalysis()
+
 filecsv_from = 'data/sample_solution.csv'
 df_result = pd.read_csv(filecsv_from)
 df_result.head()
@@ -173,11 +224,15 @@ df_result.to_csv(filecsv_to, index=False)
 
 column_list = pd.Series(['Артериальная гипертензия', 'ОНМК', 'Стенокардия, ИБС, инфаркт миокарда', 'Сердечная недостаточность',
                'Прочие заболевания сердца'])
-
+colim = column_list
+colim.index=column_list
 for col in column_list:
-    science(col, preds, filecsv_from, filecsv_to)
+    colim = colim.drop(col)
+
+    science(col, preds, filecsv_from, filecsv_to,colim)
 #for col in column_list:
 #    science(col, preds, filecsv_from, filecsv_to)
+
 
 
 
